92b8ca068c7f18f28c0a50c6acc45973ec4743b2
devmobile <antoniojoseuchoa2020@gmail.com>
2022-09-20 12:31:00 -0300

    Commit alterando item despesas

--- src/main/java/br/com/antoniojoseuchoa/infodeputados/view/AdapterDespesas.kt
+++ src/main/java/br/com/antoniojoseuchoa/infodeputados/view/AdapterDespesas.kt
@@ -1,5 +1,6 @@
 package br.com.antoniojoseuchoa.infodeputados.view
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -23,13 +24,28 @@ class AdapterDespesas(val context: Context, var list: List<Dado>): RecyclerView.
     override fun getItemCount() = list.size
 
     open class ViewHolder(val binding: ItemDespesasBinding) : RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
         fun bind(item: Dado) {
                 binding.tvAno.text = "ANO: ${item.ano}"
-                binding.tvMes.text = "MES: ${item.mes}"
+
+                when(item.mes.toInt()){
+                      1 -> { binding.tvMes.text = "MES: Janeiro"}
+                      2 -> { binding.tvMes.text = "MES: Fevereiro"}
+                      3 -> { binding.tvMes.text = "MES: Março" }
+                      4 -> { binding.tvMes.text = "MES: Abril" }
+                      5 -> { binding.tvMes.text = "MES: Maio" }
+                      6 -> { binding.tvMes.text = "MES: Junho" }
+                      7 -> { binding.tvMes.text = "MES: Julho" }
+                      8 -> { binding.tvMes.text = "MES: Agosto" }
+                      9 -> { binding.tvMes.text = "MES: Setembro"}
+                      10 -> { binding.tvMes.text = "MES: Outubro" }
+                      11 -> { binding.tvMes.text = "MES: Novembro" }
+                      12 -> { binding.tvMes.text = "MES: Dezembro" }
+                }
+
                 binding.tvTipoDespesa.text = "TIPO DESPESAS: ${item.tipoDespesa}"
                 binding.tvValor.text = "VALOR R$: ${item.valorLiquido}"
         }
     }
-
 
 }
